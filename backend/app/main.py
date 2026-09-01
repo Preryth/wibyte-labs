6c29a9dc85a9f4478a1532c322cb8710dd72e498
Preryth <preryth@gmail.com>
2026-09-01 20:43:19 +0530

    Add labs.wibyte.in as an allowed origin in main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -139,6 +139,9 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
+        "http://200.234.44.47:4173",
+        "http://labs.wibyte.in",
+        "https://labs.wibyte.in",
     ],
     allow_credentials=True,
     allow_methods=["*"],
